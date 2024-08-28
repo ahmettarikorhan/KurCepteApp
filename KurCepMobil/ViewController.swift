1aa5b6acf4ee2259ca436366c6e554d03a2fbda4
OP <proprooyuncu90@gmail.com>
2024-08-28 11:21:55 +0300

    Initial Commit

---
+++ KurCepMobil/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KurCepMobil
+//
+//  Created by mac on 28.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
