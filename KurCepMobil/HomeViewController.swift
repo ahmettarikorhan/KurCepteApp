6864636c03f3ab62de553b8ad6f34de1f08a4c1d
OP <proprooyuncu90@gmail.com>
2024-08-28 16:14:22 +0300

    second commit

--- KurCepMobil/HomeViewController.swift
+++ KurCepMobil/HomeViewController.swift
@@ -7,12 +7,32 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UITableViewDelegate,UITableViewDataSource {
+   
+    
+    
+    
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print("HomeViewControler")
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+         let cell = tableView.dequeueReusableCell(withIdentifier: "MyTableViewCell", for: indexPath) as! MyTableViewCell
+                
+        cell.imageView?.image = currencyList[indexPath.row].currencyIcon
+                cell.fromLabel.text = currencyList[indexPath.row].currencyName
+                cell.toLabel.text = "TRY"
+                cell.changeRateLabel.text = String(currencyList[indexPath.row].changeRate)
+                cell.priceLabel.text = String(currencyList[indexPath.row].price)
+        return cell
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return currencyList.count
     }
 
 
