074fee39539f19bfbb020ae84a0ad00c99360e7c
OP <proprooyuncu90@gmail.com>
2024-08-29 14:59:09 +0300

    api added

--- KurCepMobil/HomeViewController.swift
+++ KurCepMobil/HomeViewController.swift
@@ -4,7 +4,7 @@
 //
 //  Created by mac on 28.08.2024.
 //
-
+import Foundation
 import UIKit
 
 class HomeViewController: UIViewController, UITableViewDelegate,UITableViewDataSource {
@@ -23,9 +23,31 @@ class HomeViewController: UIViewController, UITableViewDelegate,UITableViewDataS
         tableView.dataSource = self
         tableView.delegate = self
         
+        let parameters = ["access_key":"229fe4c9315234288063cd59c754c0c2"]
+        var urlComponents = URLComponents(string: "https://data.fixer.io/api/latest")!
+        
+         urlComponents.queryItems = parameters.map{URLQueryItem(name: $0.key,value: $0.value)}
+        
+        guard let url = urlComponents.url else {
+            print("invalid url")
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        let task = URLSession.shared.dataTask(with: request) {data,response,error in
+            if let data = data {
+                if let dataString = String(data: data, encoding: .utf8){
+                    print("Data is in String: \(dataString)")
+                }
+            }
+            
+        }
+        task.resume()
+        
     }
     
-   
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         
